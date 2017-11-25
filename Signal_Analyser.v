eaf915e8660708a34b73ac53021e16ba5a3b1317
Flora Liu <yufang.liu@mail.utoronto.ca>
2017-11-25 15:54:31 -0500

    Modification during the lab

--- Signal_Analyser.v
+++ Signal_Analyser.v
@@ -1,39 +1,66 @@
-module Signal_Analyser (left, read_ready, clk_60hz, resetn, pitch, volumn);
+module Signal_Analyser (left, read, read_ready, clk_50, clk_60hz, resetn, pitch, vol, valid_left, count);
 
   input [23:0] left;
   input read_ready;
   input clk_60hz;
+  input clk_50;
   input resetn;
   output [1:0] pitch;
-  output volumn;
+  output vol;
+  output reg read;
+  output reg [23:0] valid_left;
+  output [14:0] count;
 
-  pitch_counter pitch_cnter(left, read_ready, resetn, pitch);
+  pitch_counter pitch_cnter(valid_left, read_ready, clk_50, resetn, pitch, count);
 
-  volumn vlm (left, read_ready, resetn, clk_60hz, volumn);
-
+  volumn vlm(valid_left, read_ready, clk_50, resetn, clk_60hz, vol);
+	always @(posedge clk_50)
+	begin
+	read = 0;
+	if(read_ready == 1) begin
+	   valid_left <= left;
+		read <= 1;
+		end
+	else
+		valid_left <= valid_left;
+	end
 
 endmodule //Signal_Analyser
 
-// output 3 states according to the privious pitch before the clock edge
-module pitch_counter (value, read_ready, resetn, out);
+module pitch_counter (value, read_ready, clk_50, resetn, out, count);
   input [23:0] value;
+  input clk_50;
   input read_ready;
   input resetn;
   output reg [1:0] out;
-
-  reg [14:0] count;
+  output reg [14:0] count;
   wire [1:0] category;
   wire sign;
+  reg presign;
 
   assign sign = value[23];
 
-  always @(posedge read_ready)
+  always @(posedge clk_50)
   begin
-    if (sign == 1'b1 | resetn == 1'b0)
+  if (resetn == 1'b0)
+		begin
       count <= 15'h0000;
-    else
+		presign <= 0;
+		end
+	 else if (sign != presign)
+		begin
+		count <= 15'h0000;
+		presign <= sign;
+		end
+	 else if (read_ready == 1)
+		begin
       count <= count + 1'b1;
+		presign <= sign;
+		end
+	 else
+		count <= count;
   end
+  
 
   // always @(posedge clk_48k)
   // begin
@@ -44,21 +71,30 @@ module pitch_counter (value, read_ready, resetn, out);
   // end
 
   always @ (posedge sign) begin
-    if (count > 15'h0004 | resetn == 1'b0)
-    // if(count > 15'h0078)
-      out <= 2'b00;
-    else if (count >= 15'h0002)
-    // else if (count >= 15'h003C)
+//    if (count > 15'h0004 | resetn == 1'b0)
+	if (resetn == 1'b0)
+		out <= 2'b00;
+    else 
+	 begin
+	 if(count > 15'h0078)
+      out <= 2'b10;
+//    else if (count >= 15'h0002)
+    else if (count >= 15'h003C)
       out <= 2'b01;
     else
       out <= 2'b11;
+	end
   end
 endmodule //pitch_counter
 
-module volumn (left, read_ready, resetn, clk_60hz, out);
+
+// output 3 states according to the privious pitch before the clock edge
+
+module volumn (left, read_ready, clk_50, resetn, clk_60hz, out);
 
   input [23:0] left;
   input read_ready;
+  input clk_50;
   input clk_60hz;
   input resetn;
   output reg out;
@@ -68,23 +104,52 @@ module volumn (left, read_ready, resetn, clk_60hz, out);
 
 
   absolute_value abs(left, abs_left);
-
-  always @ ( posedge read_ready, posedge clk_60hz) begin
-    if (clk_60hz == 1'b1 | resetn == 1'b0)
-      maximum <= 24'h000000;
-    else if (abs_left > maximum)
-      maximum <= left;
-    else
-      maximum <= maximum;
-  end
-
-  always @(posedge clk_60hz)
+  
+  always @(posedge clk_50)
   begin
-    if (maximum >= 24'h3fffff)
-      out <= 1;
-    else
-      out <= 0;
+	if(read_ready == 1)
+	begin
+		if (abs_left >= 24'h00ffff)
+		out <= 1;
+		else
+		out <= 0;
+	end
+	else
+		out <= out;
   end
+
+//  always @ ( posedge clk_50) 
+//	begin
+//		if (resetn == 0 | clk_60hz == 1'b1)
+//		 maximum <= 24'h000000;
+//		else if(read_ready == 1)
+//			begin
+//				if (abs_left > maximum)
+//						maximum <= left;
+//				else
+//						maximum <= maximum;
+//				
+//			end
+//		else
+//			maximum <= maximum;
+//  end
+////    if (resetn == 0)
+////		maximum <= 24'h000000;
+////	 else if (clk_60hz == 1'b1)
+////      maximum <= 24'h000000;
+////    else if (abs_left > maximum)
+////      maximum <= left;
+////    else
+////      maximum <= maximum;
+//
+//
+//  always @(posedge clk_60hz)
+//  begin
+//    if (maximum >= 24'h3fffff)
+//      out <= 1;
+//    else
+//      out <= 0;
+//  end
 
 endmodule //
 
@@ -99,3 +164,4 @@ module absolute_value(in, out);
       out = in;
   end
 endmodule
+
--- milestone1.v
+++ milestone1.v
@@ -1,5 +1,6 @@
 module milestone1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_XCK,
-		        AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK, AUD_ADCDAT, AUD_DACDAT, LEDR);
+		        AUD_DACLRCK, AUD_ADCLRCK, AUD_BCLK, AUD_ADCDAT, AUD_DACDAT, LEDR
+				  , HEX0, HEX1, HEX2, HEX3, HEX4, HEX5);
 
 	input CLOCK_50, CLOCK2_50;
 	input [0:0] KEY;
@@ -12,19 +13,21 @@ module milestone1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_X
 	input AUD_ADCDAT;
 	output AUD_DACDAT;
   output [9:0] LEDR;
+  output [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+  
 
 	// Local wires.
 	wire read_ready, write_ready, read, write;
 	wire [23:0] readdata_left, readdata_right;
 	wire [23:0] writedata_left, writedata_right;
-	wire reset = KEY[0];
+	wire reset = ~KEY[0];
   wire clk_60hz;
-
+	wire [23:0] valid_left;
 	/////////////////////////////////
 	// Your code goes here
 	/////////////////////////////////
 
-	assign read = 1'b1;
+
 	assign write =1'b0;
 
 /////////////////////////////////////////////////////////////////////////////////
@@ -81,12 +84,44 @@ module milestone1 (CLOCK_50, CLOCK2_50, KEY, FPGA_I2C_SCLK, FPGA_I2C_SDAT, AUD_X
 		AUD_DACDAT
 	);
 
-
+	wire [14:0] count;
+	
 	Signal_Analyser SA(
-		readdata_left, read_ready,
+		readdata_right, read, read_ready, CLOCK_50, 
 		clk_60hz, reset,
-		LEDR[1:0], LEDR[9]);
+		LEDR[1:0], LEDR[9], valid_left, count);
 
   sixtyhzcounter sc(1, CLOCK_50, reset, clk_60hz);
+  hex_decoder hd0(count[3:0], HEX0);
+  hex_decoder hd1(count[7:4], HEX1);
+	hex_decoder hd2(count[11:8], HEX2);
+	hex_decoder hd3({1'b0, count[14:12]}, HEX3);
+	hex_decoder hd4(valid_left[19:16], HEX4);
+	hex_decoder hd5(valid_left[23:20], HEX5);
+endmodule
 
+module hex_decoder(hex_digit, segments);
+    input [3:0] hex_digit;
+    output reg [6:0] segments;
+   
+    always @(*)
+        case (hex_digit)
+            4'h0: segments = 7'b100_0000;
+            4'h1: segments = 7'b111_1001;
+            4'h2: segments = 7'b010_0100;
+            4'h3: segments = 7'b011_0000;
+            4'h4: segments = 7'b001_1001;
+            4'h5: segments = 7'b001_0010;
+            4'h6: segments = 7'b000_0010;
+            4'h7: segments = 7'b111_1000;
+            4'h8: segments = 7'b000_0000;
+            4'h9: segments = 7'b001_1000;
+            4'hA: segments = 7'b000_1000;
+            4'hB: segments = 7'b000_0011;
+            4'hC: segments = 7'b100_0110;
+            4'hD: segments = 7'b010_0001;
+            4'hE: segments = 7'b000_0110;
+            4'hF: segments = 7'b000_1110;   
+            default: segments = 7'h7f;
+        endcase
 endmodule
