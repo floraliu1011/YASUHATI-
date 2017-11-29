268bb5b3153796afea701b4b0bae65a8f580fe8b
Flora Liu <yufang.liu@mail.utoronto.ca>
2017-11-29 00:43:01 -0500

    modified audio signal analyser

--- Signal_Analyser.v
+++ Signal_Analyser.v
@@ -1,17 +1,12 @@
-module Signal_Analyser (left, read, read_ready, clk_50, clk_60hz, resetn, pitch, vol, valid_left, count);
+module Signal_Analyser (left, read, read_ready, clk_50, clk_60hz, resetn, vol);
 
   input [23:0] left;
   input read_ready;
   input clk_60hz;
   input clk_50;
   input resetn;
-  output [1:0] pitch;
   output vol;
   output reg read;
-  output reg [23:0] valid_left;
-  output [14:0] count;
-
-  pitch_counter pitch_cnter(valid_left, read_ready, clk_50, resetn, pitch, count);
 
   volumn vlm(valid_left, read_ready, clk_50, resetn, clk_60hz, vol);
 	always @(posedge clk_50)
@@ -26,66 +21,6 @@ module Signal_Analyser (left, read, read_ready, clk_50, clk_60hz, resetn, pitch,
 	end
 
 endmodule //Signal_Analyser
-
-module pitch_counter (value, read_ready, clk_50, resetn, out, count);
-  input [23:0] value;
-  input clk_50;
-  input read_ready;
-  input resetn;
-  output reg [1:0] out;
-  output reg [14:0] count;
-  wire [1:0] category;
-  wire sign;
-  reg presign;
-
-  assign sign = value[23];
-
-  always @(posedge clk_50)
-  begin
-  if (resetn == 1'b0)
-		begin
-      count <= 15'h0000;
-		presign <= 0;
-		end
-	 else if (sign != presign)
-		begin
-		count <= 15'h0000;
-		presign <= sign;
-		end
-	 else if (read_ready == 1)
-		begin
-      count <= count + 1'b1;
-		presign <= sign;
-		end
-	 else
-		count <= count;
-  end
-
-
-  // always @(posedge clk_48k)
-  // begin
-  //   if (sign == 1'b1 | resetn == 1'b0)
-  //     count <= 15'h0000;
-  //   else
-  //     count <= count + 1'b1;
-  // end
-
-  always @ (posedge sign) begin
-//    if (count > 15'h0004 | resetn == 1'b0)
-	if (resetn == 1'b0)
-		out <= 2'b00;
-    else
-	 begin
-	 if(count > 15'h0078)
-      out <= 2'b10;
-//    else if (count >= 15'h0002)
-    else if (count >= 15'h003C)
-      out <= 2'b01;
-    else
-      out <= 2'b11;
-	end
-  end
-endmodule //pitch_counter
 
 
 // output 3 states according to the privious pitch before the clock edge
