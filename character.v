7ee0fc2aec13ec6625057759d412f465c873bf34
Flora Liu <yufang.liu@mail.utoronto.ca>
2017-12-06 21:30:29 -0500

    modified pixel numbers in the character

--- character.v
+++ character.v
@@ -35,7 +35,49 @@ module character (
        (increment_x == `x20 && increment_y == `y20) ||
        (increment_x == `x21 && increment_y == `y21) ||
        (increment_x == `x22 && increment_y == `y22) ||
-       (increment_x == `x23 && increment_y == `y23))
+       (increment_x == `x23 && increment_y == `y23) ||
+       (increment_x == `x24 && increment_y == `y24) ||
+       (increment_x == `x25 && increment_y == `y25) ||
+       (increment_x == `x26 && increment_y == `y26) ||
+       (increment_x == `x27 && increment_y == `y27) ||
+       (increment_x == `x28 && increment_y == `y28) ||
+       (increment_x == `x29 && increment_y == `y29) ||
+       (increment_x == `x30 && increment_y == `y30) ||
+       (increment_x == `x31 && increment_y == `y31) ||
+       (increment_x == `x32 && increment_y == `y32) ||
+       (increment_x == `x33 && increment_y == `y33) ||
+       (increment_x == `x34 && increment_y == `y34) ||
+       (increment_x == `x35 && increment_y == `y35) ||
+       (increment_x == `x36 && increment_y == `y36) ||
+       (increment_x == `x37 && increment_y == `y37) ||
+       (increment_x == `x38 && increment_y == `y38) ||
+       (increment_x == `x39 && increment_y == `y39) ||
+       (increment_x == `x40 && increment_y == `y40) ||
+       (increment_x == `x41 && increment_y == `y41) ||
+       (increment_x == `x42 && increment_y == `y42) ||
+       (increment_x == `x43 && increment_y == `y43) ||
+       (increment_x == `x44 && increment_y == `y44) ||
+       (increment_x == `x45 && increment_y == `y45) ||
+       (increment_x == `x46 && increment_y == `y46) ||
+       (increment_x == `x47 && increment_y == `y47) ||
+       (increment_x == `x48 && increment_y == `y48) ||
+       (increment_x == `x49 && increment_y == `y49) ||
+       (increment_x == `x50 && increment_y == `y50) ||
+       (increment_x == `x51 && increment_y == `y51) ||
+       (increment_x == `x52 && increment_y == `y52) ||
+       (increment_x == `x53 && increment_y == `y53) ||
+       (increment_x == `x54 && increment_y == `y54) ||
+       (increment_x == `x55 && increment_y == `y55) ||
+       (increment_x == `x56 && increment_y == `y56) ||
+       (increment_x == `x57 && increment_y == `y57) ||
+       (increment_x == `x58 && increment_y == `y58) ||
+       (increment_x == `x59 && increment_y == `y59) ||
+       (increment_x == `x60 && increment_y == `y60) ||
+       (increment_x == `x61 && increment_y == `y61) ||
+       (increment_x == `x62 && increment_y == `y62) ||
+       (increment_x == `x63 && increment_y == `y63) ||
+       (increment_x == `x64 && increment_y == `y64) ||
+       (increment_x == `x65 && increment_y == `y65))
 		 color = 3'b011;
 		else
 	   color = 3'b111;
